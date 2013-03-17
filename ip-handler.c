a647e7fbfcc4321babfbf9335c8c217fbf8bf65b
Daniel Duan <danielduan88@yahoo.com>
2013-03-16 20:13:51 -0700

    Added IP sending and ICMP sending.

--- ip-handler.c
+++ ip-handler.c
@@ -53,14 +53,105 @@ void create_icmp_packet(struct sr_instance* sr, sr_icmp_hdr_t *packet, unsigned 
 	
 	free(newICMP);
 	
-	
+void send_icmp_packet(struct sr_instance* sr,
+        sr_icmp_hdr_t* packet, 
+        unsigned int len, 
+        char* interface,
+        uint8_t type, 
+        uint8_t code) {
+ 
+  /* ethernet header */
+  sr_ethernet_hdr_t* eth_header = (sr_ethernet_hdr_t*)packet; 
+  /* ip header */
+  sr_ip_hdr_t* ip_header = (sr_ip_hdr_t*)(eth_header+1); 
+  /* outgoing interface */
+  struct sr_if* out_if = sr_get_interface(sr, interface); 
+  
+  uint8_t icmp_len;
+  uint8_t payload_len;
+  /* echo reply */
+  if (type == 0) {
+    icmp_len = ntohs(ip_header->ip_len) - sizeof(sr_ip_hdr_t );    
+    payload_len = icmp_len - sizeof(sr_icmp_hdr_t) - 4;
+  } else {
+    icmp_len = sizeof(sr_icmp_t3_hdr_t);
+    payload_len = ICMP_DATA_SIZE;
+  }
+  
+  uint8_t total_len = sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t)+icmp_len;
+  uint8_t* icmp_pkt = (uint8_t*)malloc(total_len); 
+   
+  /* generate ICMP header */  
+  sr_icmp_t3_hdr_t* icmp_header = (sr_icmp_t3_hdr_t*)(icmp_pkt+sizeof(sr_ethernet_hdr_t)+sizeof(sr_ip_hdr_t));
+  
+  memcpy(icmp_header, ip_header+1, icmp_len);
+  icmp_header->icmp_type = type;
+  icmp_header->icmp_code = code;    
+  icmp_header->icmp_sum = 0;
+  /* not echo reply */
+  if (type!=0) {
+    memcpy(icmp_header->data, ip_header, payload_len);
+  }
+  icmp_header->icmp_sum = cksum(icmp_header, sizeof(sr_icmp_t3_hdr_t)); 
+  
+  /* generate IP packet */
+  sr_ip_hdr_t* new_ip_header = (sr_ip_hdr_t*)(icmp_pkt+sizeof(sr_ethernet_hdr_t));
+  memcpy(new_ip_header, ip_header, sizeof(sr_ip_hdr_t));
+  uint16_t pk_len = sizeof(sr_ip_hdr_t)+ icmp_len;
+  new_ip_header->ip_len = htons(pk_len);
+  new_ip_header->ip_src = out_if->ip;
+  new_ip_header->ip_dst = ip_header->ip_src;
+  new_ip_header->ip_ttl = 0x40;
+  new_ip_header->ip_p = ip_protocol_icmp;
+  
+  new_ip_header->ip_sum = 0;
+  new_ip_header->ip_sum = cksum(new_ip_header, sizeof(sr_ip_hdr_t));  
 
-void send_icmp_packet() { //INCOMPLETE
+  /* generate Ethernet frame*/
+  sr_ethernet_hdr_t* new_eth_header = (sr_ethernet_hdr_t*)icmp_pkt;
+  new_eth_header->ether_type = htons(ethertype_ip);  
+  
+  /* send ICMP packet*/
+  send_ip_packet(sr, icmp_pkt, total_len, interface, true);
+  /* free extra memory */
+  free(icmp_pkt);
 }
 
 
-void send_ip_packet() {
-	//Daniel's code goes here
+void send_ip_packet(struct sr_instance* sr,
+        uint8_t * packet,
+        unsigned int len,
+        char* interface/* lent */,
+        bool ICMP) {
+  sr_ethernet_hdr_t* eth_header = (sr_ethernet_hdr_t*)packet;  
+  sr_ip_hdr_t* ip_header = (sr_ip_hdr_t*)(eth_header+1);  
+  /* check dest IP in routing table */ 
+  struct sr_rt* routing_index = check_rtable(sr, ntohl(ip_header->ip_dst));
+  if (routing_index) {
+    uint32_t next_hop_ip = ntohl(routing_index->gw.s_addr);
+    char* next_hop_if = routing_index->interface;
+    /* check ARP in cache */
+    struct sr_arpentry *entry = sr_arpcache_lookup(&sr->cache, next_hop_ip);
+    if (entry && entry->valid) {
+      struct sr_if* out_if = sr_get_interface(sr, next_hop_if);
+      /* update ethernet header */
+      memcpy(eth_header->ether_shost, out_if->addr, ETHER_ADDR_LEN);
+      memcpy(eth_header->ether_dhost, entry->mac, ETHER_ADDR_LEN); 
+      /* send packet to next hop*/ 
+      sr_send_packet(sr, packet, len, next_hop_if);
+    } else {
+      /* save packet in the request queue */
+      struct sr_arpreq * req =  sr_arpcache_queuereq(&sr->cache, next_hop_ip, packet, len, next_hop_if);
+      request_arp(sr, req);
+    }
+    free(entry);
+  } else {
+    /* ICMP destination unreachable*/
+    if (!ICMP) {
+      send_icmp(sr, packet, len, interface, 3, 0);
+    }
+  }
+ 
 }
 
 
@@ -109,7 +200,10 @@ void handle_ip_packet(struct sr_instance * sr, EthernetFrame * frame, char * int
 		
 		}
 	}
-	else printf("Daniel's Section\n");
+	else //printf("Daniel's Section\n");
+	{
+		send_ip_packet(sr, frame, len, interface, false);
+	}
 		
 	/*****************************************************************/printf("###: Checkpoint 0\n");
 	
